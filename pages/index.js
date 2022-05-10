e42e91aeb55292a554695e9edc5c22a3a27679a3
Saikat Shankhari <saikat.shankhari@gmail.com>
2022-05-10 16:14:37 +0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import { Flex, Text, Button } from "@chakra-ui/react"
+import Head from "next/head"
+import { useMoralis } from "react-moralis"
+
+export default function Home() {
+  const {isAuthenticated, authenticate} = useMoralis();
+  if (!isAuthenticated) {
+    return (
+      <>
+        <Head>
+          <title>Login | Dashboard3</title>
+        </Head>
+        <Flex 
+          direction="column" 
+          justifyContent="center" 
+          alignItems="center"
+          width="100vw" 
+          height="100vh" 
+          bgGradient="linear(to-br, teal.400, purple.300)"
+          >
+          <Text fontSize="5xl" fontWeight="bold" color="white">Dashboard3</Text>
+          <Button colorScheme="purple" size="lg" mt="6" 
+            onClick={() => authenticate({})}
+            >Login with Metamask</Button>
+        </Flex>
+      </>
+    )
+  } else {
+    return (
+      <div>
+        <h1>You are logged in successfully!</h1>
+      </div>
+    )
+  }
+}
